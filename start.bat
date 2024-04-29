28ea72e88720990ed94d0efe2b0cf0cb6359fa20
Mr-Zanzibar <104436099+Mr-Zanzibar@users.noreply.github.com>
2024-04-29 23:58:05 +0200

    Update
    - Added a "pause" that allows the cmd not to close by itself
    
    - New credits (redirect to this page)
    
    - Now if you enter a wrong number the code will not stop
    
    - Coloured CMD

--- start.bat
+++ start.bat
@@ -1,11 +1,12 @@
 @echo off
 
-echo  Select an option:
-echo  1. Run the Python code
-echo  2. Compile the Python code into an .exe file
-echo  3. Display the credits list
+echo Select an option:
+echo 1. Run the Python code
+echo 2. Compile the Python code into an .exe file
+echo 3. Display the credits list
 
-set /p choice=Enter the option number:
+:option
+set /p choice=Enter the option number: 
 
 if "%choice%"=="1" (
     pip install ecdsa base58
@@ -15,14 +16,16 @@ if "%choice%"=="1" (
     pyinstaller --onefile miner.py
     rmdir /s /q build
     del /s /q miner.py.spec
-    echo The .exe file has been created successfully.
+    cls
+    powershell -Command Write-Host "The .exe file has been created successfully." -ForegroundColor Green
+    pause
 ) else if "%choice%"=="3" (
-    echo Credits:
-    echo - Mr-Zanzibar: https://github.com/Mr-Zanzibar/Fake-BTC
-    echo - Discord: don.zanzibar
+    echo Mr-Zanzibar
+    start https://github.com/Mr-Zanzibar/Fake-BTC
+    timeout /t 5 /nobreak >nul
+    goto :option
 ) else (
-    echo Invalid option.
+    powershell -Command Write-Host "Invalid option." -ForegroundColor Red
+    timeout /t 2 /nobreak >nul
+    goto :option
 )
-
-pause
-
